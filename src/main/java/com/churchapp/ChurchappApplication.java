7422123476920b4d7b5d4ed9bddf950c78081efe
Anderson F Silva <andersonferreira.1992@gmail.com>
2018-07-08 23:06:18 -0300

    Projeto criado

---
+++ src/main/java/com/churchapp/ChurchappApplication.java
@@ -0,0 +1,12 @@
+package com.churchapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChurchappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChurchappApplication.class, args);
+	}
+}
---
+++ src/main/java/com/churchapp/domain/Usuario.java
@@ -0,0 +1,132 @@
+package com.churchapp.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class Usuario implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer codigoUsuario;
+	private String nomeUsuario;
+	private String login;
+	private String senha;
+	private String tipoUsuario;
+	private String sexo;
+	private Date dataNascimento;
+	private String nickname;
+	
+	public Usuario(){}
+
+	public Usuario(Integer codigoUsuario, String nomeUsuario, String login, String senha, String tipoUsuario,
+			String sexo, Date dataNascimento, String nickname) {
+		super();
+		this.codigoUsuario = codigoUsuario;
+		this.nomeUsuario = nomeUsuario;
+		this.login = login;
+		this.senha = senha;
+		this.tipoUsuario = tipoUsuario;
+		this.sexo = sexo;
+		this.dataNascimento = dataNascimento;
+		this.nickname = nickname;
+	}
+
+	public Integer getCodigoUsuario() {
+		return codigoUsuario;
+	}
+
+	public void setCodigoUsuario(Integer codigoUsuario) {
+		this.codigoUsuario = codigoUsuario;
+	}
+
+	public String getNomeUsuario() {
+		return nomeUsuario;
+	}
+
+	public void setNomeUsuario(String nomeUsuario) {
+		this.nomeUsuario = nomeUsuario;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getTipoUsuario() {
+		return tipoUsuario;
+	}
+
+	public void setTipoUsuario(String tipoUsuario) {
+		this.tipoUsuario = tipoUsuario;
+	}
+
+	public String getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((codigoUsuario == null) ? 0 : codigoUsuario.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		if (codigoUsuario == null) {
+			if (other.codigoUsuario != null)
+				return false;
+		} else if (!codigoUsuario.equals(other.codigoUsuario))
+			return false;
+		return true;
+	}
+	
+	
+	
+	
+
+}
